bad390e3789f4f6e6136c0ae6805c558acc1a8a6
Parithi-ios <parithivigneshwaran.r2023eee@sece.ac.in>
2025-12-05 15:24:31 +0530

    Add files via upload

---
+++ Day3_Tasks/script.js
@@ -0,0 +1,13 @@
+var a=10;
+console.log("The value of a is"+a);
+
+var a=20;
+console.log("The new value of a is"+a);
+
+a=30;
+console.log("TThe updated value of a is"+a);
+
+let b=15;
+console.log("The value of b is"+b);
+
+
